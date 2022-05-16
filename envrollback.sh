c3dcc42ee9a7a5e526faef396173f4c6493eeb56
ingdestino <afrancescon@fbk.eu>
2022-05-16 14:06:42 +0200

    env vars add/remove scripts for test

---
+++ envrollback.sh
@@ -0,0 +1,9 @@
+# Ctrl-h export -> unset 
+# Ctrl-h =.* -> ""
+
+unset LOG_LEVEL
+unset SUPPORT_MESSAGE_RESEND_TIMEOUT
+unset API_PORT
+unset RESPONSE_ROUTER_EP
+unset RESPONSE_ROUTER_API
+unset RECEIVER_BROKER_TOPIC
